f8a67ee8c7e0950475be8c6af8d42c2f7417f249
Alex McLain <alex@alexmclain.com>
2016-02-15 23:09:09 -0800

    Integrated Soundweb encoder into Qsys system. Fixed pin assignments.

--- fpga/bss_hps.v
+++ fpga/bss_hps.v
@@ -1,4 +1,5 @@
-module bss_hps(// HPS
+module bss_hps(
+  // HPS
   inout         HPS_CONV_USB_N,
   output [14:0] HPS_DDR3_ADDR,
   output  [2:0] HPS_DDR3_BA,
@@ -46,57 +47,128 @@ module bss_hps(// HPS
   inout   [7:0] HPS_USB_DATA,
   input         HPS_USB_DIR,
   input         HPS_USB_NXT,
-  output        HPS_USB_STP,// Clock
+  output        HPS_USB_STP,
+
+  // Clock
   input FPGA_CLK1_50,
   input FPGA_CLK2_50,
-  input FPGA_CLK3_50,// ADC
+  input FPGA_CLK3_50,
+
+  // ADC
   output ADC_CONVST,
   output ADC_SCK,
   output ADC_SDI,
-  input  ADC_SDO,// Switches
-  input [3:0] SW// Keys
-  input [1:0] KEY,// LEDs
-  output [7:0] LED,
+  input  ADC_SDO,
+
+  // Switches
+  input [3:0] SW,
+
+  // Keys
+  input [1:0] KEY,
+
+  // LEDs
+  output [7:0] LED
 );
 
+wire [31:0] bss_ctl_0;
+reg  [31:0] bss_ctl_1;
+
+wire [31:0] bss_data_in_0;
+wire [31:0] bss_data_in_1;
+wire [31:0] bss_data_in_2;
+wire [31:0] bss_data_in_3;
+wire [31:0] bss_data_out_0;
+wire [31:0] bss_data_out_1;
+wire [31:0] bss_data_out_2;
+wire [31:0] bss_data_out_3;
+wire [31:0] bss_data_out_4;
+wire [31:0] bss_data_out_5;
+wire [31:0] bss_data_out_6;
+wire [31:0] bss_data_out_7;
+
 soc_system u0 (
-  .clk_clk                                   (FPGA_CLK1_50),                                             // clk.clk
-  .reset_reset_n                             (1'b1),                                                     // reset.reset_n
-  .memory_mem_a                              (HPS_DDR3_ADDR),                                            // memory.mem_a
-  .memory_mem_ba                             (HPS_DDR3_BA),                                              // .mem_ba
-  .memory_mem_ck                             (HPS_DDR3_CK_P),                                            // .mem_ck
-  .memory_mem_ck_n                           (HPS_DDR3_CK_N),                                            // .mem_ck_n
-  .memory_mem_cke                            (HPS_DDR3_CKE),                                             // .mem_cke
-  .memory_mem_cs_n                           (HPS_DDR3_CS_N),                                            // .mem_cs_n
-  .memory_mem_ras_n                          (HPS_DDR3_RAS_N),                                           // .mem_ras_n
-  .memory_mem_cas_n                          (HPS_DDR3_CAS_N),                                           // .mem_cas_n
-  .memory_mem_we_n                           (HPS_DDR3_WE_N),                                            // .mem_we_n
-  .memory_mem_reset_n                        (HPS_DDR3_RESET_N),                                         // .mem_reset_n
-  .memory_mem_dq                             (HPS_DDR3_DQ),                                              // .mem_dq
-  .memory_mem_dqs                            (HPS_DDR3_DQS_P),                                           // .mem_dqs
-  .memory_mem_dqs_n                          (HPS_DDR3_DQS_N),                                           // .mem_dqs_n
-  .memory_mem_odt                            (HPS_DDR3_ODT),                                             // .mem_odt
-  .memory_mem_dm                             (HPS_DDR3_DM),                                              // .mem_dm
-  .memory_oct_rzqin                          (HPS_DDR3_RZQ),                                             // .oct_rzqin
-  .hps_0_f2h_cold_reset_req_reset_n          (1'b0),                                                     // hps_0_f2h_cold_reset_req.reset_n
-  .hps_0_f2h_debug_reset_req_reset_n         (1'b0),                                                     // hps_0_f2h_debug_reset_req.reset_n
-  .hps_0_f2h_warm_reset_req_reset_n          (1'b0),                                                     // hps_0_f2h_warm_reset_req.reset_n
-  .hps_0_h2f_reset_reset_n                   (1'b0),                                                     // hps_0_h2f_reset.reset_n
-  .bss_ctl_0_external_connection_export      (<connected-to-bss_ctl_external_connection_export>),        // bss_ctl_0_external_connection.export
-  .bss_ctl_1_external_connection_export      (<connected-to-bss_ctl_1_external_connection_export>)       // bss_ctl_1_external_connection.export
-  .bss_data_in_0_external_connection_export  (<connected-to-bss_data_in_0_external_connection_export>),  // bss_data_in_0_external_connection.export
-  .bss_data_in_1_external_connection_export  (<connected-to-bss_data_in_1_external_connection_export>),  // bss_data_in_1_external_connection.export
-  .bss_data_in_2_external_connection_export  (<connected-to-bss_data_in_2_external_connection_export>),  // bss_data_in_2_external_connection.export
-  .bss_data_in_3_external_connection_export  (<connected-to-bss_data_in_3_external_connection_export>),  // bss_data_in_3_external_connection.export
-  .bss_data_out_0_external_connection_export (<connected-to-bss_data_out_0_external_connection_export>), // bss_data_out_0_external_connection.export
-  .bss_data_out_1_external_connection_export (<connected-to-bss_data_out_1_external_connection_export>), // bss_data_out_1_external_connection.export
-  .bss_data_out_2_external_connection_export (<connected-to-bss_data_out_2_external_connection_export>), // bss_data_out_2_external_connection.export
-  .bss_data_out_3_external_connection_export (<connected-to-bss_data_out_3_external_connection_export>), // bss_data_out_3_external_connection.export
-  .bss_data_out_4_external_connection_export (<connected-to-bss_data_out_4_external_connection_export>), // bss_data_out_4_external_connection.export
-  .bss_data_out_5_external_connection_export (<connected-to-bss_data_out_5_external_connection_export>), // bss_data_out_5_external_connection.export
-  .bss_data_out_6_external_connection_export (<connected-to-bss_data_out_6_external_connection_export>), // bss_data_out_6_external_connection.export
-  .bss_data_out_7_external_connection_export (<connected-to-bss_data_out_7_external_connection_export>), // bss_data_out_7_external_connection.export
-  .bss_data_out_8_external_connection_export (<connected-to-bss_data_out_8_external_connection_export>), // bss_data_out_8_external_connection.export
+  .clk_clk                                   (FPGA_CLK1_50),     // clk.clk
+  .reset_reset_n                             (1'b1),             // reset.reset_n
+  .memory_mem_a                              (HPS_DDR3_ADDR),    // memory.mem_a
+  .memory_mem_ba                             (HPS_DDR3_BA),      // .mem_ba
+  .memory_mem_ck                             (HPS_DDR3_CK_P),    // .mem_ck
+  .memory_mem_ck_n                           (HPS_DDR3_CK_N),    // .mem_ck_n
+  .memory_mem_cke                            (HPS_DDR3_CKE),     // .mem_cke
+  .memory_mem_cs_n                           (HPS_DDR3_CS_N),    // .mem_cs_n
+  .memory_mem_ras_n                          (HPS_DDR3_RAS_N),   // .mem_ras_n
+  .memory_mem_cas_n                          (HPS_DDR3_CAS_N),   // .mem_cas_n
+  .memory_mem_we_n                           (HPS_DDR3_WE_N),    // .mem_we_n
+  .memory_mem_reset_n                        (HPS_DDR3_RESET_N), // .mem_reset_n
+  .memory_mem_dq                             (HPS_DDR3_DQ),      // .mem_dq
+  .memory_mem_dqs                            (HPS_DDR3_DQS_P),   // .mem_dqs
+  .memory_mem_dqs_n                          (HPS_DDR3_DQS_N),   // .mem_dqs_n
+  .memory_mem_odt                            (HPS_DDR3_ODT),     // .mem_odt
+  .memory_mem_dm                             (HPS_DDR3_DM),      // .mem_dm
+  .memory_oct_rzqin                          (HPS_DDR3_RZQ),     // .oct_rzqin
+  .hps_0_f2h_cold_reset_req_reset_n          (1'b0),             // hps_0_f2h_cold_reset_req.reset_n
+  .hps_0_f2h_debug_reset_req_reset_n         (1'b0),             // hps_0_f2h_debug_reset_req.reset_n
+  .hps_0_f2h_warm_reset_req_reset_n          (1'b0),             // hps_0_f2h_warm_reset_req.reset_n
+  .hps_0_h2f_reset_reset_n                   (1'b0),             // hps_0_h2f_reset.reset_n
+  .bss_ctl_0_external_connection_export      (bss_ctl_0),        // bss_ctl_0_external_connection.export
+  .bss_ctl_1_external_connection_export      (bss_ctl_1),        // bss_ctl_1_external_connection.export
+  .bss_data_in_0_external_connection_export  (bss_data_in_0),    // bss_data_in_0_external_connection.export
+  .bss_data_in_1_external_connection_export  (bss_data_in_1),    // bss_data_in_1_external_connection.export
+  .bss_data_in_2_external_connection_export  (bss_data_in_2),    // bss_data_in_2_external_connection.export
+  .bss_data_in_3_external_connection_export  (bss_data_in_3),    // bss_data_in_3_external_connection.export
+  .bss_data_out_0_external_connection_export (bss_data_out_0),   // bss_data_out_0_external_connection.export
+  .bss_data_out_1_external_connection_export (bss_data_out_1),   // bss_data_out_1_external_connection.export
+  .bss_data_out_2_external_connection_export (bss_data_out_2),   // bss_data_out_2_external_connection.export
+  .bss_data_out_3_external_connection_export (bss_data_out_3),   // bss_data_out_3_external_connection.export
+  .bss_data_out_4_external_connection_export (bss_data_out_4),   // bss_data_out_4_external_connection.export
+  .bss_data_out_5_external_connection_export (bss_data_out_5),   // bss_data_out_5_external_connection.export
+  .bss_data_out_6_external_connection_export (bss_data_out_6),   // bss_data_out_6_external_connection.export
+  .bss_data_out_7_external_connection_export (bss_data_out_7),   // bss_data_out_7_external_connection.export
+);
+
+soundweb_encoder u1 (
+  .command   (bss_data_in_0[7:0]),
+  .address_0 (bss_data_in_1[7:0]),
+  .address_1 (bss_data_in_1[15:8]),
+  .address_2 (bss_data_in_1[23:16]),
+  .address_3 (bss_data_in_1[31:24]),
+  .address_4 (bss_data_in_2[7:0]),
+  .address_5 (bss_data_in_2[15:8]),
+  .sv_0      (bss_data_in_2[23:16]),
+  .sv_1      (bss_data_in_2[31:24]),
+  .data_0    (bss_data_in_3[7:0]),
+  .data_1    (bss_data_in_3[15:8]),
+  .data_2    (bss_data_in_3[23:16]),
+  .data_3    (bss_data_in_3[31:24]),
+
+  .packet_0  (bss_data_out_0[7:0]),
+  .packet_1  (bss_data_out_0[15:8]),
+  .packet_2  (bss_data_out_0[23:16]),
+  .packet_3  (bss_data_out_0[31:24]),
+  .packet_4  (bss_data_out_1[7:0]),
+  .packet_5  (bss_data_out_1[15:8]),
+  .packet_6  (bss_data_out_1[23:16]),
+  .packet_7  (bss_data_out_1[31:24]),
+  .packet_8  (bss_data_out_2[7:0]),
+  .packet_9  (bss_data_out_2[15:8]),
+  .packet_10 (bss_data_out_2[23:16]),
+  .packet_11 (bss_data_out_2[31:24]),
+  .packet_12 (bss_data_out_3[7:0]),
+  .packet_13 (bss_data_out_3[15:8]),
+  .packet_14 (bss_data_out_3[23:16]),
+  .packet_15 (bss_data_out_3[31:24]),
+  .packet_16 (bss_data_out_4[7:0]),
+  .packet_17 (bss_data_out_4[15:8]),
+  .packet_18 (bss_data_out_4[23:16]),
+  .packet_19 (bss_data_out_4[31:24]),
+  .packet_20 (bss_data_out_5[7:0]),
+  .packet_21 (bss_data_out_5[15:8]),
+  .packet_22 (bss_data_out_5[23:16]),
+  .packet_23 (bss_data_out_5[31:24]),
+  .packet_24 (bss_data_out_6[7:0]),
+  .packet_25 (bss_data_out_6[15:8]),
+  .packet_26 (bss_data_out_6[23:16]),
+  .packet_27 (bss_data_out_6[31:24]),
+  .packet_28 (bss_data_out_7[7:0])
 );
 
 endmodule
---
+++ fpga/src/soundweb_encoder.v
@@ -0,0 +1,199 @@
+module soundweb_encoder(
+  input [7:0] command,
+  input [7:0] address_0,
+  input [7:0] address_1,
+  input [7:0] address_2,
+  input [7:0] address_3,
+  input [7:0] address_4,
+  input [7:0] address_5,
+  input [7:0] sv_0,
+  input [7:0] sv_1,
+  input [7:0] data_0,
+  input [7:0] data_1,
+  input [7:0] data_2,
+  input [7:0] data_3,
+
+  output [7:0] packet_0,
+  output [7:0] packet_1,
+  output [7:0] packet_2,
+  output [7:0] packet_3,
+  output [7:0] packet_4,
+  output [7:0] packet_5,
+  output [7:0] packet_6,
+  output [7:0] packet_7,
+  output [7:0] packet_8,
+  output [7:0] packet_9,
+  output [7:0] packet_10,
+  output [7:0] packet_11,
+  output [7:0] packet_12,
+  output [7:0] packet_13,
+  output [7:0] packet_14,
+  output [7:0] packet_15,
+  output [7:0] packet_16,
+  output [7:0] packet_17,
+  output [7:0] packet_18,
+  output [7:0] packet_19,
+  output [7:0] packet_20,
+  output [7:0] packet_21,
+  output [7:0] packet_22,
+  output [7:0] packet_23,
+  output [7:0] packet_24,
+  output [7:0] packet_25,
+  output [7:0] packet_26,
+  output [7:0] packet_27,
+  output [7:0] packet_28
+);
+
+parameter STX = 8'h02;
+parameter ETX = 8'h03;
+parameter ACK = 8'h06;
+parameter NAK = 8'h15;
+parameter ESC = 8'h1B;
+
+wire [7:0] address [0:5];
+wire [7:0] sv      [0:1];
+wire [7:0] data    [0:3];
+reg  [7:0] checksum;
+
+reg  [7:0] output_buffer [0:28];
+
+assign address[0] = address_0;
+assign address[1] = address_1;
+assign address[2] = address_2;
+assign address[3] = address_3;
+assign address[4] = address_4;
+assign address[5] = address_5;
+
+assign sv[0] = sv_0;
+assign sv[1] = sv_1;
+
+assign data[0] = data_0;
+assign data[1] = data_1;
+assign data[2] = data_2;
+assign data[3] = data_3;
+
+assign packet_0  = output_buffer[0];
+assign packet_1  = output_buffer[1];
+assign packet_2  = output_buffer[2];
+assign packet_3  = output_buffer[3];
+assign packet_4  = output_buffer[4];
+assign packet_5  = output_buffer[5];
+assign packet_6  = output_buffer[6];
+assign packet_7  = output_buffer[7];
+assign packet_8  = output_buffer[8];
+assign packet_9  = output_buffer[9];
+assign packet_10 = output_buffer[10];
+assign packet_11 = output_buffer[11];
+assign packet_12 = output_buffer[12];
+assign packet_13 = output_buffer[13];
+assign packet_14 = output_buffer[14];
+assign packet_15 = output_buffer[15];
+assign packet_16 = output_buffer[16];
+assign packet_17 = output_buffer[17];
+assign packet_18 = output_buffer[18];
+assign packet_19 = output_buffer[19];
+assign packet_20 = output_buffer[20];
+assign packet_21 = output_buffer[21];
+assign packet_22 = output_buffer[22];
+assign packet_23 = output_buffer[23];
+assign packet_24 = output_buffer[24];
+assign packet_25 = output_buffer[25];
+assign packet_26 = output_buffer[26];
+assign packet_27 = output_buffer[27];
+assign packet_28 = output_buffer[28];
+
+wire [7:0] input_buffer  [0:13];
+reg  [5:0] output_index  [0:14];
+reg  [5:0] output_offset [0:14];
+
+parameter COMMAND   = 0;
+parameter ADDRESS_0 = 1;
+parameter ADDRESS_1 = 2;
+parameter ADDRESS_2 = 3;
+parameter ADDRESS_3 = 4;
+parameter ADDRESS_4 = 5;
+parameter ADDRESS_5 = 6;
+parameter SV_0      = 7;
+parameter SV_1      = 8;
+parameter DATA_0    = 9;
+parameter DATA_1    = 10;
+parameter DATA_2    = 11;
+parameter DATA_3    = 12;
+parameter CHECKSUM  = 13;
+parameter ETX_INDEX = 14;
+
+assign input_buffer[COMMAND]   = command;
+assign input_buffer[ADDRESS_0] = address[0];
+assign input_buffer[ADDRESS_1] = address[1];
+assign input_buffer[ADDRESS_2] = address[2];
+assign input_buffer[ADDRESS_3] = address[3];
+assign input_buffer[ADDRESS_4] = address[4];
+assign input_buffer[ADDRESS_5] = address[5];
+assign input_buffer[SV_0]      = sv[0];
+assign input_buffer[SV_1]      = sv[1];
+assign input_buffer[DATA_0]    = data[0];
+assign input_buffer[DATA_1]    = data[1];
+assign input_buffer[DATA_2]    = data[2];
+assign input_buffer[DATA_3]    = data[3];
+assign input_buffer[CHECKSUM]  = checksum;
+
+integer i;
+integer j;
+
+always @(*)
+begin
+  for (i = COMMAND; i <= ETX_INDEX; i = i + 1) begin
+    output_index[i] = 0;
+    output_offset[i] = 0;
+  end
+
+  for (i = 0; i <= 28; i = i + 1) begin
+    output_buffer[i] = 0;
+  end
+
+  checksum = 8'h00;
+
+  for (i = COMMAND; i <= DATA_3; i = i + 1) begin
+    checksum = checksum ^ input_buffer[i];
+  end
+
+  output_buffer[0] = STX;
+
+  for (i = COMMAND; i <= CHECKSUM; i = i + 1) begin
+    // Offset is +1 for STX in output_buffer.
+    output_index[i] = 1 + i + output_offset[i];
+
+    if (is_reserved_byte(input_buffer[i])) begin
+      output_buffer[output_index[i]] = ESC;
+      output_buffer[output_index[i] + 1] = input_buffer[i] + 8'h80;
+
+      for (j = i + 1; j <= ETX_INDEX; j = j + 1) begin
+        output_offset[j] = output_offset[j] + 1;
+      end
+    end else begin
+      output_buffer[output_index[i]] = input_buffer[i];
+    end
+  end
+
+  output_index[ETX_INDEX] = 1 + ETX_INDEX + output_offset[ETX_INDEX];
+  output_buffer[output_index[ETX_INDEX]] = ETX;
+end
+
+function is_reserved_byte;
+input [7:0] byte;
+begin
+  if (
+    byte == STX ||
+    byte == ETX ||
+    byte == ACK ||
+    byte == NAK ||
+    byte == ESC
+  ) begin
+    is_reserved_byte = 1'b1;
+  end else begin
+    is_reserved_byte = 1'b0;
+  end
+end
+endfunction
+
+endmodule
