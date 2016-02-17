4479030589c9427a69b8424ab60be680bbcda978
Alex McLain <alex@alexmclain.com>
2016-02-16 21:22:26 -0800

    Set up HPS boilerplate.

---
+++ hps/src/main.c
@@ -0,0 +1,21 @@
+#define soc_cv_av
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "hwlib.h"
+#include "soc_cv_av/socal/socal.h"
+#include "soc_cv_av/socal/hps.h"
+#include "soc_cv_av/socal/alt_gpio.h"
+#include "hps.h"
+
+#define HW_REGS_BASE (ALT_STM_OFST)
+#define HW_REGS_SPAN (0x04000000)
+#define HW_REGS_MASK (HW_REGS_SPAN - 1)
+
+int main() {
+  printf("It works.\n");
+
+  return 0;
+}
